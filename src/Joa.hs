5aa7001719ed36b0e3dfc7586357460ff6fbaad0
Yann Cardon <ycardon@gmail.com>
2020-10-09 09:18:39 +0200

    more point free

--- src/Joa.hs
+++ src/Joa.hs
@@ -22,25 +22,25 @@ joa command = do
       putStrLn $ "result:  " ++ showRoll (applyDefence attackRoll defenceRoll)
     else putStrLn $ showRoll attackRoll
 
--- count the number of a given face in a roll result
+-- count the number of a given face in a roll
 count :: Face -> Roll -> Int
-count face roll = length (filter (== face) roll)
+count face = length . filter (== face)
 
--- cancel roll faces by an amount of shield count (fold version)
+-- cancel roll faces by an amount of shield count [fold version]
 cancel :: Face -> (Roll, Int) -> (Roll, Int)
 cancel face (roll, shieldCount) = foldr f ([], shieldCount) roll
   where
     f x (xs, n) = if n <= 0 then (x : xs, 0) else if x == face then (xs, n - 1) else (x : xs, n)
 
--- apply defence shields on the attack and remove shields from the attack)
+-- apply defence shields on the attack and remove unrelevant faces from the attack
 applyDefence :: Roll -> Roll -> Roll
 applyDefence attack defence = filter (/= Blank) . filter (/= Shield) $ roll
   where
     (roll, _) = cancel Push . cancel Disrupt . cancel Kill $ (attack, count Shield defence)
 
--- frequency of each face in the roll (using a map)
+-- frequency of each face in the roll [map variant]
 frequency :: [Face] -> [(Face, Int)]
-frequency roll = toList (fromListWith (+) [(face, 1) | face <- roll])
+frequency roll = toList . fromListWith (+) $ [(face, 1) | face <- roll]
 
 -- pretty print a roll
 showRoll :: Roll -> String
@@ -48,7 +48,7 @@ showRoll = intercalate " | " . map (\(f, n) -> show n ++ " " ++ show f) . freque
 
 --------- implementation variants ---------
 
--- -- cancel roll faces by an amount of shield count (recursive version)
+-- -- cancel roll faces by an amount of shield count [recursive variant]
 -- cancel' :: Face -> (Roll, Int) -> (Roll, Int)
 -- cancel' _ ([], n) = ([], n)
 -- cancel' face (x : xs, n)
@@ -60,6 +60,6 @@ showRoll = intercalate " | " . map (\(f, n) -> show n ++ " " ++ show f) . freque
 
 -- import Data.List (group, sort)
 
--- -- frequency of each face in the roll (using list group)
+-- -- frequency of each face in the roll [list group variant]
 -- frequency' :: [Face] -> [(Face, Int)]
 -- frequency' = map (\(x : xs) -> (x, length xs + 1)) . group . sort
